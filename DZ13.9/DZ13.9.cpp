c48e57573dbb32e2caedfa7e79d6207274fb1b19
Andrei <dasd_78@mail.ru>
2023-04-27 20:13:14 +0300

    DZ 13.9

--- DZ13.9/DZ13.9.cpp
+++ DZ13.9/DZ13.9.cpp
@@ -1,20 +1,47 @@
-﻿// DZ13.9.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include "Connections.h"
 
-#include <iostream>
+using namespace std;
 
-int main()
+int main(int argc, const char* argv[])
 {
-    std::cout << "Hello World!\n";
+    Connections c;
+
+    // добавляем вершины
+    c.addPeople("name0");
+    c.addPeople("name1");
+    c.addPeople("name2");
+    c.addPeople("name3");
+    c.addPeople("name4");
+    c.addPeople("name5");
+    c.addPeople("name6");
+    c.addPeople("name7");
+    c.addPeople("name8");
+    c.addPeople("name9");
+
+
+    // добавляем ребра
+    c.addMeet(0, 1);
+    c.addMeet(1, 2);
+    c.addMeet(2, 3);
+    c.addMeet(3, 4);
+    c.addMeet(4, 5);
+    c.addMeet(5, 6);
+    c.addMeet(6, 7);
+    c.addMeet(7, 8);
+    c.addMeet(8, 9);
+
+    for (int i = 0; i < c.getPeopleCount() - 1; i++)
+    {
+        for (int j = i + 1; j < c.getPeopleCount(); j++)
+        {
+            int distance = c.findMinWayDFS(i, j);
+            if (distance == 3)
+            {
+                cout << c.getPeopleName(i) << " дружит c " << c.getPeopleName(j) << endl;
+            }
+        }
+    }
+
+    return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
